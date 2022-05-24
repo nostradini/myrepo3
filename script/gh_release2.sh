2edc12c9798c37723ef86a2e9c113926487e1a02
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-24 15:55:14 -0700

    26

--- script/gh_release2.sh
+++ script/gh_release2.sh
@@ -16,8 +16,7 @@ curl \
 -H "Authorization: token $token" \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/$user/$repo/releases  \
--d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "release again using script","draft":fals>
-
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
 
 
 # command="curl -s -o release.json -w '%{http_code}' \
