ec3644f9c4467b55b261f1f37a2e7d9922e5ca6c
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-24 15:59:53 -0700

    27

--- script/gh_release2.sh
+++ script/gh_release2.sh
@@ -1,5 +1,5 @@
 
-#!/usr/bin/bash
+!/usr/bin/bash
 # UpdatedVer=$(cat ./VERSION)
 user="nostradini"
 repo="myrepo3"
@@ -16,23 +16,4 @@ curl \
 -H "Authorization: token $token" \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/$user/$repo/releases  \
--d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
-
-
-# command="curl -s -o release.json -w '%{http_code}' \
-#          --request POST \
-#          --header 'authorization: token $token' \
-#          --header 'content-type: application/json' \
-#          --data '{"tag_name": "${tag}"}' \
-#          https://api.github.com/repos/$user/$repo/releases"
-# http_code=`eval $command`
-# if [ $http_code == "201" ]; then
-#     echo "created release:"
-#     cat release.json
-# else
-#     echo "create release failed with code '$http_code':"
-#     cat release.json
-#     echo "command:"
-#     echo $command
-#     # return 1
-# fi
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": ":bug: release again using script","draft":false,"prerelease":false,"generate_release_notes":true}'
