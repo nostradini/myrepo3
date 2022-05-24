4a71ff3c5897fd7b49e0fcd21e1052c2fc6b67fb
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-24 00:28:49 -0700

    change

--- script/gh_release2.sh
+++ script/gh_release2.sh
@@ -1,25 +1,26 @@
-create_release() {
-    user="nostradini"
-    repo="myrepo3"
-    token=$1
-    tag=$2
 
-    command="curl -s -o release.json -w '%{http_code}' \
+#!/usr/bin/bash
+
+user="nostradini"
+repo="myrepo3"
+token=$1
+tag=$2
+
+command="curl -s -o release.json -w '%{http_code}' \
          --request POST \
          --header 'authorization: Bearer ${token}' \
          --header 'content-type: application/json' \
          --data '{\"tag_name\": \"${tag}\"}' \
          https://api.github.com/repos/$user/$repo/releases"
-    http_code=`eval $command`
-    if [ $http_code == "201" ]; then
-        echo "created release:"
-        cat release.json
-    else
-        echo "create release failed with code '$http_code':"
-        cat release.json
-        echo "command:"
-        echo $command
-        return 1
-    fi
-}
-create_release()
+http_code=`eval $command`
+if [ $http_code == "201" ]; then
+    echo "created release:"
+    cat release.json
+else
+    echo "create release failed with code '$http_code':"
+    cat release.json
+    echo "command:"
+    echo $command
+    return 1
+fi
+
