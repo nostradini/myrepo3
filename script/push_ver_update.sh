c8d9b44936f2f35d890a3f1a71d48c0835b8fe69
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-25 16:30:43 -0700

    gft

--- script/push_ver_update.sh
+++ script/push_ver_update.sh
@@ -16,8 +16,8 @@ content=$(echo $content | tr -d ' ')
 content=\"${content}\"
 echo "Content is = " $content
 
-# curl -i -X PUT \
-# -H "Authorization: token $Env_Token" \
-# -H "Accept: application/vnd.github.v3+json" \
-# -d '{ "path":"VERSION","message":"[JOB] Push version","content":'$content',"branch":"main","sha":'$Repo_SHA' }' \
-#  https://api.github.com/repos/nostradini/myrepo3/contents/VERSION
+curl -i -X PUT \
+-H "Authorization: token $Env_Token" \
+-H "Accept: application/vnd.github.v3+json" \
+-d '{ "path":"VERSION","message":"[JOB] Push version","content":'$content',"branch":"main","sha":'$Repo_SHA' }' \
+ https://api.github.com/repos/nostradini/myrepo3/contents/VERSION
