44916c63f304fb280cc820c87ddc20f190f2e41a
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-24 18:26:03 -0700

    save

--- script/gh_release.sh
+++ script/gh_release.sh
@@ -2,9 +2,12 @@
 !/usr/bin/bash
 user="$(git log -n 1 --pretty=format:%an)"
 repo="myrepo3"
+# target="$(git symbolic-ref --short HEAD)"
+com_hash="$(git rev-parse --short HEAD)"
 token=$1
 tag=$2
 
+echo "user " $user
 echo "tag" $tag
 vtag="v$tag"
 tag=\"${tag}\"
@@ -16,4 +19,4 @@ curl \
 -H "Authorization: token $token" \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/repos/$user/$repo/releases  \
--d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "## :bug: release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "## :bug: \n $com_hash - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
