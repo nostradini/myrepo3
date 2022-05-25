fd5acfac32a785468ff38a419c280a73940cb9de
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-24 17:51:57 -0700

    41

--- script/gh_release.sh
+++ script/gh_release.sh
@@ -1,47 +1,19 @@
-#!/bin/bash
 
-MESSAGE="0"
-VERSION="0.2.1"
-DRAFT="false"
-PRE="false"
-BRANCH="main"
-ACCESS_TOKEN=$1
+!/usr/bin/bash
+user="nostradini"
+repo="myrepo3"
+token=$1
+tag=$2
 
-# get repon name and owner
-# CD /myrepo3
-REPO_REMOTE=$(git config --get remote.origin.url)
+echo "tag" $tag
+vtag="v$tag"
+tag=\"${tag}\"
+vtag=\"${vtag}\"
+echo $tag $vtag
 
-if [ -z $REPO_REMOTE ]; then
-	echo "Not a git repository"
-	exit 1
-fi
-
-REPO_NAME="myrepo3"	#$(basename -s .git $REPO_REMOTE)
-REPO_OWNER=$(git config --get user.name)
-
-# get args
-while getopts v:m:b:draft:pre: option
-do
-	case "${option}"
-		in
-		v) VERSION="$OPTARG";;
-		m) MESSAGE="$OPTARG";;
-		b) BRANCH="$OPTARG";;
-		draft) DRAFT="true";;
-		pre) PRE="true";;
-	esac
-done
-if [ $VERSION == "0" ]; then
-	echo "Usage: git-release -v <version> [-b <branch>] [-m <message>] [-draft] [-pre]"
-	exit 1
-fi
-
-# set default message
-if [ "$MESSAGE" == "0" ]; then
-	MESSAGE=$(printf "Release of version %s" $VERSION)
-fi
-
-
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "v%s","body": "%s","draft": %s,"prerelease": %s}' "$VERSION" "$BRANCH" "$VERSION" "$MESSAGE" "$DRAFT" "$PRE" )
-API_RESPONSE_STATUS=$(curl -H "Authorization: token $ACCESS_TOKEN" --data "$API_JSON" -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases?access_token=$ACCESS_TOKEN)
-echo "$API_RESPONSE_STATUS"
+curl \
+-X POST \
+-H "Authorization: token $token" \
+-H "Accept: application/vnd.github.v3+json" \
+https://api.github.com/repos/$user/$repo/releases  \
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "# :bug: release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
