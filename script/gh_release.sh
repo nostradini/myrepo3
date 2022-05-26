f0525d89dfd669e4031f9387786af1c9193da6b7
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-25 20:30:25 -0700

    hope

--- script/gh_release.sh
+++ script/gh_release.sh
@@ -28,9 +28,9 @@ tag=\"${tag}\"
 vtag=\"${vtag}\"
 echo $tag $vtag
 
-# curl \
-# -X POST \
-# -H "Authorization: token $token" \
-# -H "Accept: application/vnd.github.v3+json" \
-# https://api.github.com/repos/$user/$repo/releases  \
-# -d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### '$ENV_GM' \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
+curl \
+-X POST \
+-H "Authorization: token $token" \
+-H "Accept: application/vnd.github.v3+json" \
+https://api.github.com/repos/$user/$repo/releases  \
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### '$STG_GM' \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
