875994ec4a9ca94ba2f008647b61c192a7ee6f54
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-25 19:36:45 -0700

    give it a try #minor

--- script/gh_release.sh
+++ script/gh_release.sh
@@ -6,7 +6,6 @@ com_hash="$(git rev-parse --short HEAD)"
 token=$1
 tag=$ENV_VER
 
-echo "gh_sh_ver= " $gh_sh_ver
 echo "END_GM= " $ENV_GM
 echo "ENV_VER= " $ENV_VER
 
@@ -18,9 +17,9 @@ tag=\"${tag}\"
 vtag=\"${vtag}\"
 echo $tag $vtag
 
-# curl \
-# -X POST \
-# -H "Authorization: token $token" \
-# -H "Accept: application/vnd.github.v3+json" \
-# https://api.github.com/repos/$user/$repo/releases  \
-# -d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### '$gitmojiko' Bug Fixes \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
+curl \
+-X POST \
+-H "Authorization: token $token" \
+-H "Accept: application/vnd.github.v3+json" \
+https://api.github.com/repos/$user/$repo/releases  \
+-d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### '$ENV_GM' Bug Fixes \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
