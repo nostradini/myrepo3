cb2cc3364142e61fc09b19c3a0377f1af1b40dbe
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-25 20:08:13 -0700

    fsdgdsfhd

--- script/gh_release.sh
+++ script/gh_release.sh
@@ -8,18 +8,23 @@ tag=$ENV_VER
 
 echo "END_GM= " $ENV_GM
 echo "ENV_VER= " $ENV_VER
-
 echo "user= " $user
 echo "tag = " $ENV_VER
+
+gm_type="$(cut -d'.' -f1 <<< "$ENV_GM")"
+gm_desc1="$(cut -d'.' -f2 <<< "$ENV_GM")"
+gm_desc2="$(cut -d'.' -f3 <<< "$ENV_GM")"
+
+echo "split - $gm_type  $gm_desc1 $gm_desc2"
 
 vtag="v$tag"
 tag=\"${tag}\"
 vtag=\"${vtag}\"
 echo $tag $vtag
 
-curl \
--X POST \
--H "Authorization: token $token" \
--H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/$user/$repo/releases  \
--d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### $ENV_GM \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
+# curl \
+# -X POST \
+# -H "Authorization: token $token" \
+# -H "Accept: application/vnd.github.v3+json" \
+# https://api.github.com/repos/$user/$repo/releases  \
+# -d '{"tag_name": '$tag' ,"target_commitish":"main" , "name": '$vtag' ,"body": "### $ENV_GM \n '$com_hash' - release again using script","draft":false,"prerelease":false,"generate_release_notes":false}'
