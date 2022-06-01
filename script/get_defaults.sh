a039318ec857c6edf66f4f153cfed00951f725bc
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-31 23:31:05 -0700

    ssss

--- script/get_defaults.sh
+++ script/get_defaults.sh
@@ -1,9 +1,10 @@
 # !/usr/bin/bash
 user="$(git log -n 1 --pretty=format:%an)"
 repo="myrepo3"
+
 targetD=$(curl \
-        -H "Accept: application/vnd.github.v3+json" \
-        https://api.github.com/repos/$user/$repo/releases/latest | jq .created_at)
+-H "Accept: application/vnd.github.v3+json" \
+https://api.github.com/repos/$user/$repo/releases/latest | jq .created_at)
 
 arrCom=()
 while IFS= read -r line; do
