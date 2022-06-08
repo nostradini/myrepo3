5d2d6cf2902b38cfbd0c9b0f5bd9037fcc2139bb
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-08 02:37:37 -0700

    part2 added chnagelog creation #patch

--- script/checkpoints.sh
+++ script/checkpoints.sh
@@ -1,6 +1,7 @@
 # !/usr/bin/bash
 gUSER="$(git log -n 1 --pretty=format:%an)"
 lastCommit=$(git log --format=%B -n 1 HEAD)
+repo=$REPO
 echo "Last commit = $lastCommit" 
 echo "::set-output name=LASTCOM::$lastCommit"
 echo "Git User = $gUSER" 
@@ -10,6 +11,7 @@ d1=$(date +"%Y-%m-%d")
 d2=$(date +"%T")
 d2=$(echo $d2 | tr -d ':')
 varDate="$d1-$d2"
+echo "Generated Date= $varDate"
 echo "::set-output name=envDATE::$varDate"
 
 targetD=$(curl \
