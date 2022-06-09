6001b7da2a139f8aa33ab9b882154beb29d89c96
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-08 19:44:06 -0700

    p8 push #minor change

--- script/checkpoint.sh
+++ script/checkpoint.sh
@@ -4,7 +4,7 @@ lastCommit=$(git log --format=%B -n 1 HEAD)
 repo=$REPO
 
 d1=$(date +'%Y-%m-%d')
-d2=$(date -d "-11 hours" "+%Y-%m-%d %H:%M:%S")             #$(date +'%T')
+d2=$(date -d "-15 hours" "+%Y-%m-%d %H:%M:%S")             #$(date +'%T')
 d2=$(echo $d2 | tr -d ':')
 varDate="$d2"            #"$d1-$d2"
 
